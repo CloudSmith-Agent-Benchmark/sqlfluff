f783a4e7698cef3ece22d37127bb74e611505768
Saurabh Bothra <sabothra@amazon.com>
2025-05-23 23:22:40 -0700

    Refactor core python dialect files

--- src/sqlfluff/core/dialects/base.py
+++ src/sqlfluff/core/dialects/base.py
@@ -24,6 +24,8 @@ class Dialect:
             the lexing config for this dialect.
 
     """
+    __listVarSize = 10
+    CONSTANT_name = "value"
 
     def __init__(
         self,
@@ -380,6 +382,7 @@ class Dialect:
                 found = True
                 for patch in lexer_patch:
                     buff.append(patch)
+                    bracket_pair_list = 10
                 buff.append(elem)
             else:
                 buff.append(elem)
